b42df6f0f0811a95a4bc10a1715811fa7b309ec0
Grovre <50428844+Grovre@users.noreply.github.com>
2022-03-14 02:48:00 -0500

    emergency unload, static safeLocation

--- src/main/java/me/grovre/voidescape/VoidEscape.java
+++ src/main/java/me/grovre/voidescape/VoidEscape.java
@@ -1,20 +1,30 @@
 package me.grovre.voidescape;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class VoidEscape extends JavaPlugin {
 
     public static VoidEscape plugin;
+    public static Location safeLocation;
 
     public static VoidEscape getPlugin() {
         return plugin;
+    }
+
+    public static void emergencyUnload() {
+        System.err.println("Disabling VoidEscape...");
+        Bukkit.getPluginManager().disablePlugin(plugin);
     }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
-        
+        ConfigUtils config = new ConfigUtils();
+        safeLocation = config.getSafeLocation();
+
     }
 
     @Override
